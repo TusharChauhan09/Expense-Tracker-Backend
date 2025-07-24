6d3d9041c51bea8f47c4c876ec4fe892bbaf0847
Tushar Chauhan <tusharchauhan0912@gmail.com>
2025-07-24 21:16:11 +0530

    cron job recall

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import job from "./config/cron";
 
 import rateLimiter from "./middleware/rateLimiter";
 
@@ -9,11 +10,18 @@ dotenv.config();
 
 const app = express();
 
+if(process.env.NODE_ENV === "production") job.start
+
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
 app.use(rateLimiter);
+
+app.get('/api/recall',(req,res)=>{
+  res.status(200).json({status: "ok"});
+})
 
 app.use('/api/transactions', transactionsRouter);
 
